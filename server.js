fb8e04fda18d9131b1bf81dfb2548e9ab578ff88
Daniel Luz <danielluz1995@gmail.com>
2021-01-06 08:12:10 -0300

    Add login view.

--- server.js
+++ server.js
@@ -5,20 +5,31 @@ const { default: createShopifyAuth  } = require('@shopify/koa-shopify-auth');
 const dotenv = require('dotenv');
 const { verifyRequest  } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
-
-dotenv.config();
+const router = require('koa-router');
+const render = require('koa-ejs');
+const path = require('path');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev  });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY  } = process.env;
+
+dotenv.config();
+
+const _ = router()
+
+_.get('/login', async (ctx, next) => {
+  await ctx.render('login')
+})
 
 app.prepare().then(() => {
   const server = new Koa();
   server.use(session({ sameSite: 'none', secure: true  }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  server.use(_.routes())
 
   server.use(
     createShopifyAuth({
@@ -38,6 +49,13 @@ app.prepare().then(() => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
+  });
+
+  render(server, {
+      root: path.join(__dirname, 'view'),
+      viewExt: 'html',
+      cache: false,
+      debug: true
   });
 
   server.listen(port, () => {
