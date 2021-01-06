918cf042b2ecf401d6223c876f3153ba14720ed3
Daniel Luz <danielluz1995@gmail.com>
2021-01-06 12:35:01 -0300

    Remove NextJS.

--- pages/alexa-account-link.js
+++
@@ -1,9 +0,0 @@
-const Login = () => (
-  <form action="/auth" method="GET">
-    <input type="text" name="shop" />
-    <input type="submit" />
-  </form>
-);
-
-export default Login;
-
--- server.js
+++ server.js
@@ -1,57 +1,61 @@
 require('isomorphic-fetch');
 const Koa = require('koa');
-const next = require('next');
 const { default: createShopifyAuth  } = require('@shopify/koa-shopify-auth');
 const dotenv = require('dotenv');
+dotenv.config();
+
 const { verifyRequest  } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
 const koaRouter = require('koa-router');
+const render = require('koa-ejs');
 const path = require('path');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY  } = process.env;
 
-dotenv.config();
-
 const router = koaRouter()
+const memoryState = {}
 
-router.get('/alexa-account-link', async (ctx) => {
-  await app.render(ctx.req, ctx.res, '/alexa-account-link', ctx.query)
-  ctx.respond = false
-})
+const server = new Koa();
+server.keys = [SHOPIFY_API_SECRET_KEY];
+server.use(session({ sameSite: 'none', secure: true  }, server));
 
-router.get('(.*)', async ctx => {
-  await handle(ctx.req, ctx.res);
-  ctx.respond = false;
+render(server, {
+  root: path.join(__dirname, 'view'),
+  viewExt: 'html',
+  cache: false,
+  debug: false
 });
 
-app.prepare().then(() => {
-  const server = new Koa();
-  server.use(session({ sameSite: 'none', secure: true  }, server));
-  server.keys = [SHOPIFY_API_SECRET_KEY];
+router
+  .get('/shop-login', async (ctx) => {
+    await ctx.render('shop-login', { oauth: ctx.query })
+  })
+  .get('/alexa-account-link', async (ctx) => {
+    const { shop, state, redirect_uri, token_type } = ctx.query
+    memoryState[shop] = { state, redirect_uri }
+    ctx.redirect(`/auth?shop=${shop}`);
+  })
 
-  server.use(
-    createShopifyAuth({
-      apiKey: SHOPIFY_API_KEY,
-      secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products'],
-      afterAuth(ctx) {
-        const { shop, accessToken  } = ctx.session;
+server.use(router.routes())
+server.use(
+  createShopifyAuth({
+    apiKey: SHOPIFY_API_KEY,
+    secret: SHOPIFY_API_SECRET_KEY,
+    scopes: ['read_products'],
+    afterAuth(ctx) {
+      const { shop, accessToken  } = ctx.session;
+      const { redirect_uri, state } = memoryState[shop]
 
-        ctx.redirect('/');
-      },
-    }),
-  );
+      ctx.redirect(`${redirect_uri}?state=${state}&access_token=${accessToken}&token_type=bearer`);
+    },
+  }),
+);
 
-  server.use(router.routes())
+server.use(verifyRequest());
 
-  server.use(verifyRequest());
-
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+server.listen(port, () => {
+  console.log(`> Ready on http://localhost:${port}`);
 });
